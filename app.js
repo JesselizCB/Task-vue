3dea03c6b118bfae6b9447bd2781be3831e3b5eb
Jesseliz <choque101108@gmail.com>
2019-12-04 17:38:43 -0500

    corrigiendo metodos

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 const app = new Vue({
-el: '#app',
-data: {
-titulo: 'Reg. de tareas con vue',
-tareas: [],
-nuevaTarea: ''
-},
-methods:{
-    agregarTarea: function(){
-        // console.log(this.nuevaTarea);
-        this.tareas.push({
-            nombre: this.nuevaTarea,
-            estado: false
-        });
-        // console.log(this.tareas);
-        this.nuevaTarea = '';
-        localStorage.setItem('gym-vue', JSON.stringfy(this.tareas));
+  el: '#app',
+  data: {
+    titulo: 'Reg. de tareas con vue',
+    tareas: [],
+    nuevaTarea: ''
+  },
+  methods: {
+    agregarTarea () {
+      // console.log(this.nuevaTarea);
+      this.tareas.push({
+        nombre: this.nuevaTarea,
+        estado: false
+      });
+      // console.log(this.tareas);
+      this.nuevaTarea = '';
+      localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
     },
-    editarTarea: function(index){
-        if (this.tareas[index].estado === false) {
-            this.tareas[index].estado = true;
-        } else {
-            this.tareas[index].estado = false;
-        }
-        
-        localStorage.setItem('gym-vue', JSON.stringfy(this.tareas));
+    editarTarea (index) {
+      if (this.tareas[index].estado) {
+        this.tareas[index].estado = false;
+      } else {
+        this.tareas[index].estado = true;
+      }
+
+      localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
     },
-    eliminarTarea: function(index){
-        this.tareas.splice(index,1);
-        localStorage.setItem('gym-vue', JSON.stringfy(this.tareas));
+    eliminarTarea (index) {
+      this.tareas.splice(index, 1);
+      localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
     }
-},
-created: function(){
- let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
-console.log(datosDB);
-if (datosDB === null) {
-    this.tareas = [];
-}else{
-    this.tareas = datosDB;
-}
-}
+  },
+  created () {
+    let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+    if (datosDB === null) {
+      this.tareas = [];
+    } else {
+      this.tareas = datosDB;
+    }
+  }
 });
